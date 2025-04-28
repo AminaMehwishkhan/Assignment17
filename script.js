ececd626ddf2b558144c8d04a6c1ec7312376622
Amina Mehwish <157866327+AminaMehwishkhan@users.noreply.github.com>
2025-04-28 06:41:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let radius = parseFloat(prompt("Enter the radius of the circle:"));
+
+let diameter = 2 * radius;
+let circumference = 2 * Math.PI * radius;
+let area = Math.PI * radius * radius;
+
+alert("Diameter: " + diameter + 
+      "\nCircumference: " + circumference + 
+      "\nArea: " + area);
